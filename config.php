ebcaf1b46c1dedec9e4074908ba5213767ebd091
Irvan Sanjaya <arvanizm29@gmail.com>
2024-05-13 17:03:39 +0700

    upload
    upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php $config = array(
+    "database_type"=>"mysql",
+    "database_name"=>"itmanajemen",
+    "server"=>"localhost",
+    "username"=>"root",
+    "password"=>"",
+    "charset"=>"utf8",
+    "port"=>3306,
+    "encryption_key"=>"59b565c60d5429344ba54c4abf538da3264a950c5b16523f0b946ffbf1809909",
+	"encryption_iv"=>"d9a5ab2780bf5111a17a689108939b5562f596298a3d36d82d26460e058c6387" ); ?>
